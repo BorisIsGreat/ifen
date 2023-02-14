b2ffa5db30fdafd134e08fcf178ae7b8b2c20982
BorisIsGreat <125251676+BorisIsGreat@users.noreply.github.com>
2023-02-14 13:52:05 +0100

    Update yoav-kadosh-3dcarousel-script.js

--- yoav-kadosh-3dcarousel-script.js
+++ yoav-kadosh-3dcarousel-script.js
@@ -2,39 +2,51 @@ import React, { useState } from 'https://cdn.skypack.dev/react';
 import ReactDOM from 'https://cdn.skypack.dev/react-dom';
 import { TiChevronLeftOutline, TiChevronRightOutline } from 'https://cdn.skypack.dev/react-icons/ti';
 
-const CARDS = 10;
+const CARDS = 2;
 const MAX_VISIBILITY = 1;
 
-const Card = ({ title, content }) => /*#__PURE__*/
-React.createElement("div", { className: "card" }, /*#__PURE__*/
-React.createElement("h2", null, title), /*#__PURE__*/
-React.createElement("p", null, content));
-
-
+const Card = ({ title, content, text }) => (
+  <div className="card">
+    <h2>{title}</h2>
+    <p>{content}</p>
+    <p>{text}</p>
+  </div>
+);
 
 const Carousel = ({ children }) => {
-  const [active, setActive] = useState(2);
+  const [active, setActive] = useState(0);
   const count = React.Children.count(children);
 
-  return /*#__PURE__*/(
-    React.createElement("div", { className: "carousel" },
-    active > 0 && /*#__PURE__*/React.createElement("button", { className: "nav left", onClick: () => setActive(i => i - 1) }, /*#__PURE__*/React.createElement(TiChevronLeftOutline, null)),
-    React.Children.map(children, (child, i) => /*#__PURE__*/
-    React.createElement("div", { className: "card-container", style: {
-        '--active': i === active ? 1 : 0,
-        '--offset': (active - i) / 3,
-        '--direction': Math.sign(active - i),
-        '--abs-offset': Math.abs(active - i) / 3,
-        'pointer-events': active === i ? 'auto' : 'none',
-        'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
-        'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block' } },
-
-    child)),
-
-
-    active < count - 1 && /*#__PURE__*/React.createElement("button", { className: "nav right", onClick: () => setActive(i => i + 1) }, /*#__PURE__*/React.createElement(TiChevronRightOutline, null))));
-
-
+  return (
+    <div className="carousel">
+      {active > 0 && (
+        <button className="nav left" onClick={() => setActive(i => i - 1)}>
+          <TiChevronLeftOutline />
+        </button>
+      )}
+      {React.Children.map(children, (child, i) => (
+        <div
+          className="card-container"
+          style={{
+            '--active': i === active ? 1 : 0,
+            '--offset': (active - i) / 3,
+            '--direction': Math.sign(active - i),
+            '--abs-offset': Math.abs(active - i) / 3,
+            'pointer-events': active === i ? 'auto' : 'none',
+            'opacity': Math.abs(active - i) >= MAX_VISIBILITY ? '0' : '1',
+            'display': Math.abs(active - i) > MAX_VISIBILITY ? 'none' : 'block'
+          }}
+        >
+          {child}
+        </div>
+      ))}
+      {active < count - 1 && (
+        <button className="nav right" onClick={() => setActive(i => i + 1)}>
+          <TiChevronRightOutline />
+        </button>
+      )}
+    </div>
+  );
 };
 
 const App = () => (
@@ -54,6 +66,4 @@ const App = () => (
   </div>
 );
 
-ReactDOM.render( /*#__PURE__*/
-React.createElement(App, null),
-document.body);
+ReactDOM.render(<App />, document.body);
